69abeaef8c468d5dd397a52ef66eeb7c88c26a1b
Jason Jordan <jasonajordan0@gmail.com>
2020-10-01 21:21:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,13 +1,16 @@
 # Write your methods here
 
-def loop_message_five_times
-  
+def loop_message_five_times(string)
+  counter = 0
+  while counter < 5 do
+    puts string 
+  end
+end
 
-
-def loop_message_n_times
+def loop_message_n_times(string, num)
   
   
 def output_array
   
   
-def 
+def return_string_array
